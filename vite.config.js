491aac415c65a1e6aa87b4d4c1e0c4cc9d0af7b3
abraham <gutierrezabraham2018@outlook.com>
2024-12-05 12:31:08 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 //https://vite.dev/config/
-{/*export default defineConfig({
+export default defineConfig({
   plugins: [react()],
-})*/}
+})
 
-export default defineConfig({ base: "https://AbrahamGutierrez92.github.io/pruebaOutdoor", plugins: [react()], }) 
+
